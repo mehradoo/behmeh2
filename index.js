24fa860c55be02c073e332bee14e759c0a74e074
Ubuntu <ubuntu@ip-172-31-12-107.ec2.internal>
2017-05-12 09:40:37 +0000

    1st commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var phantom = require("phantom");
+
+exports.get = function(event, context, callback) {
+
+var _ph, _page, _outObj;
+
+phantom.create().then(ph => {
+    _ph = ph;
+    return _ph.createPage();
+}).then(page => {
+    _page = page;
+    return _page.open('https://stackoverflow.com/');
+}).then(status => {
+    console.log(status);
+    return _page.property('content')
+}).then(content => {
+    console.log(content);
+    _page.close();
+    _ph.exit();
+}).catch(e => console.log(e)); 
+
+
+}
+
