af9268b8b78a927cf2b1c84c6933817afa513403
Mehrad Sadegh <mehrad.sadegh@intelligentpathways.com.au>
2017-05-13 23:55:12 +1000

    +Removed phantomjs dependency temporarily

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-var phantom = require("phantom");
-var cheerio = require("cheerio");
+var request = require("request"),
+    cheerio = require("cheerio");
 
 exports.get = (event, context, callback) => {
 
-    var _phantom, _page;
     var url = event.url;
     var selector = event.selector;
+    var result = {};
 
-    phantom.create().then(ph => {
-        _phantom = ph;
-        return _phantom.createPage();
-    }).then(page => {
-        _page = page;
-        return _page.open(url);
-    }).then(status => {
-        console.log(status);
-        return _page.property('content');
-    }).then(content => {
-        var $ = cheerio.load(content);
-        var result = {};
+    console.log("URL: " + url);
+    console.log("selector: " + selector);
 
-        if (selector) {
-            var items = [];
-            $(selector).each(function (i, elem) {
-                items.push($(this).text());
-            });
-            result.result = items;
+    request(url, function (error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body);
+            if (selector) {
+                var items = [];
+                $(selector).each(function () {
+                    items.push($(this).text());
+                });
+                result.result = items;
 
+            } else {
+                result.result = content;
+            }
+
+            callback(null, result);
         } else {
-            result.result = content;
+            console.log("ERROR: " + error);
+            callback(new Error('Something went wrong'));
         }
-
-        _page.close();
-        _phantom.exit();
-
-        callback(null, result);
-    }).catch(e => {
-        console.log(e);
-        callback(new Error('Something went wrong'));
     });
 
 };
