10a70a96701b22584178eba0837409a2b577e64d
Mehrad Sadegh <mehrad.sadegh@intelligentpathways.com.au>
2017-05-13 22:16:49 +1000

    +cheerio for supporting jquery/css selectors

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 var phantom = require("phantom");
+var cheerio = require("cheerio");
 
-exports.get = function(event, context, callback) {
+exports.get = (event, context, callback) => {
 
-var _ph, _page, _outObj;
+    var _phantom, _page;
+    var url = event.url;
+    var selector = event.selector;
 
-phantom.create().then(ph => {
-    _ph = ph;
-    return _ph.createPage();
-}).then(page => {
-    _page = page;
-    return _page.open('https://stackoverflow.com/');
-}).then(status => {
-    console.log(status);
-    return _page.property('content')
-}).then(content => {
-    console.log(content);
-    _page.close();
-    _ph.exit();
-}).catch(e => console.log(e)); 
+    phantom.create().then(ph => {
+        _phantom = ph;
+        return _phantom.createPage();
+    }).then(page => {
+        _page = page;
+        return _page.open(url);
+    }).then(status => {
+        console.log(status);
+        return _page.property('content');
+    }).then(content => {
+        var $ = cheerio.load(content);
+        var result = {};
 
+        if (selector) {
+            var items = [];
+            $(selector).each(function (i, elem) {
+                items.push($(this).text());
+            });
+            result.result = items;
 
-}
+        } else {
+            result.result = content;
+        }
+
+        _page.close();
+        _phantom.exit();
+
+        callback(null, result);
+    }).catch(e => {
+        console.log(e);
+        callback(new Error('Something went wrong'));
+    });
+
+};
 
