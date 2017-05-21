2857d6ac2f4118c51fc93c1642d4953e55811f37
Mehrad Sadegh <mehrad.sadegh@intelligentpathways.com.au>
2017-05-21 16:34:36 +1000

    +fixed 'Malformed Lambda proxy response'

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
+'use strict';
+
 var request = require("request"),
     cheerio = require("cheerio");
 
 exports.get = (event, context, callback) => {
 
+    console.log('Event: ' + JSON.stringify(event));
+    console.log('Context: ' + JSON.stringify(context));
+
     context.callbackWaitsForEmptyEventLoop = false;
 
-    var url = event.url;
-    var selector = event.selector;
-    var result = {};
+    var url = event.queryStringParameters.url;
+    var selector = event.queryStringParameters.selector;
 
     console.log("URL: " + url);
     console.log("selector: " + selector);
 
     request(url, function (error, response, body) {
+
+        //TODO: validation
         if (!error) {
             var $ = cheerio.load(body);
-            if (selector) {
-                var items = [];
-                $(selector).each(function () {
-                    items.push($(this).text());
-                });
-                result.result = items;
+            var items = [];
+            $(selector).each(function () {
+                items.push($(this).text());
+            });
 
-            } else {
-                result.result = content;
-            }
-
-            console.log("Result: " + JSON.stringify(result));
-            callback(null, result);
+            var response = {
+                "isBase64Encoded": false,
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json; charset=utf-8"},
+                "body": JSON.stringify(items)
+            };
+            console.log("Response: " + JSON.stringify(response));
+            callback(null, response);
         } else {
             console.log("ERROR: " + error);
-            callback(new Error('Something went wrong'));
+            callback({
+                "statusCode": 500,
+                "body": "Something went wrong:" + JSON.stringify(error)
+            });
         }
     });
 
